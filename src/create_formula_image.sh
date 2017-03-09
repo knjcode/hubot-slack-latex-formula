2d0e429025ae541c0b4b06fde668e8b9d28c67f4
knjcode <knjcode@gmail.com>
2017-03-10 00:54:01 +0900

    Validate png file

--- src/create_formula_image.sh
+++ src/create_formula_image.sh
@@ -6,3 +6,4 @@ FORMULA="$2" envsubst < template.tex > target.tex
 pdflatex -halt-on-error target.tex
 pdfcrop target.pdf target-crop.pdf
 convert -density 1000 target-crop.pdf result.png
+file result.png | grep "PNG image data"
