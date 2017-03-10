937de69a128b1c4dbaa25e3b3ad6c5ac2e60a3fe
knjcode <knjcode@gmail.com>
2017-03-10 12:39:00 +0900

    Remove transparency

--- src/create_formula_image.sh
+++ src/create_formula_image.sh
@@ -5,5 +5,5 @@ cd "$1"
 FORMULA="$2" envsubst < template.tex > target.tex
 pdflatex -halt-on-error target.tex
 pdfcrop target.pdf target-crop.pdf
-convert -density 1000 target-crop.pdf result.png
+convert -density 1000 -background white -alpha remove target-crop.pdf result.png
 file result.png | grep "PNG image data"
